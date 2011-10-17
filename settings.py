ee6a10a44a3cd459c2f85eca17b16d77d8963f29
Derek Stegelman <dstegelman@gmail.com>
2011-10-16 21:10:22 -0500

    Additional settings needed for cookbook

---
+++ settings.py
@@ -0,0 +1,12 @@
+# Extra settings for Cookbook app
+
+AUTH_PROFILE_MODULE = 'cookbook.Profile'
+
+TWITTER_ACCESS_TOKEN = "yours"
+TWITTER_ACCESS_SECRET = "yours"
+
+TWITTER_CONSUMER_KEY = "Yours"
+TWITTER_CONSUMER_SECRET = "yours"
+
+BITLY_API_KEY = "yours"
+BITLY_USERNAME = "username"
